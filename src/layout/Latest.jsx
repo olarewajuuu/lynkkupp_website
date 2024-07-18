09720b0c51f389552fae7b5079148d721cbe3641
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-07-18 11:09:59 +0100

    add the google play button on mobile

--- src/layout/Latest.jsx
+++ src/layout/Latest.jsx
@@ -41,6 +41,57 @@ const Latest = () => {
 									{image.subtitle}
 								</span>
 							</div>
+							{/* Slider Control */}
+							{index === 0 && (
+								<button
+									type="button"
+									className="hidden md:flex absolute top-1/2 transform -translate-y-1/2 -left-8 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
+									data-carousel-prev
+								>
+									<span className="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-[#d9d9d9] group-hover:bg-[#d9d9d9]/50 group-focus:ring-4 group-focus:ring-[#d9d9d9]/70 group-focus:outline-none">
+										<svg
+											className="w-5 h-5 text-[#55bfea] sm:w-6 sm:h-6"
+											fill="none"
+											stroke="currentColor"
+											viewBox="0 0 24 24"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												strokeWidth="2"
+												d="M15 19l-7-7 7-7"
+											></path>
+										</svg>
+										<span className="hidden">Previous</span>
+									</span>
+								</button>
+							)}
+							{index === 1 && (
+								<button
+									type="button"
+									className="hidden md:flex absolute top-1/2 transform -translate-y-1/2 -right-8 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
+									data-carousel-next
+								>
+									<span className="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-[#d9d9d9] group-hover:bg-[#d9d9d9]/50 group-focus:ring-4 group-focus:ring-[#d9d9d9]/70 group-focus:outline-none">
+										<svg
+											className="w-5 h-5 text-[#55bfea] sm:w-6 sm:h-6"
+											fill="none"
+											stroke="currentColor"
+											viewBox="0 0 24 24"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												strokeWidth="2"
+												d="M9 5l7 7-7 7"
+											></path>
+										</svg>
+										<span className="hidden">Next</span>
+									</span>
+								</button>
+							)}
 						</div>
 					</div>
 				))}
