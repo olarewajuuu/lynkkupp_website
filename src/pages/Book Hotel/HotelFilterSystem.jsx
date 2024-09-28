53d394835bf874bdcdc31f452a1bbb89044ff214
AdamAbdulkareem <dmabdulkareem@gmail.com>
2024-09-28 17:57:14 +0100

    feat: Add more filter functionalities - Filter by Amenities - Filter by number of ratings

--- src/pages/Book Hotel/HotelFilterSystem.jsx
+++ src/pages/Book Hotel/HotelFilterSystem.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import * as Slider from "@radix-ui/react-slider"
 import "./HotelFilterSystem.css"
-const HotelFilterSystem = ({ setSortBy }) => {
+const HotelFilterSystem = ({ setSortBy, setSelectedAmenities, setSelectedRatings }) => {
 
     const minPrice = 16000
     const [maxPrice, setMaxPrice] = useState([270000]);
@@ -11,6 +11,23 @@ const HotelFilterSystem = ({ setSortBy }) => {
     }
     const handlePriceChange = (newValue) => {
         setMaxPrice(newValue[0]);
+    }
+
+    const handleAmenitiesChange = (event) => {
+        const { value, checked } = event.target;
+        if (checked) {
+            setSelectedAmenities((prev) => [...prev, value]);
+        } else {
+            setSelectedAmenities((prev) => prev.filter((amenity) => amenity !== value));
+        }
+    }
+    const handleRatingSelection = (e) => {
+        const value = parseInt(e.target.value);
+        if (e.target.checked) {
+            setSelectedRatings(prev => [...prev, value]);
+        } else {
+            setSelectedRatings(prev => prev.filter(rating => rating !== value));
+        }
     }
 
     return (
@@ -23,6 +40,11 @@ const HotelFilterSystem = ({ setSortBy }) => {
                 <li onClick={() => handleSortClick('user-rating')}>User ratings (Highest first)</li>
             </ul>
 
+
+            <h2>Filter By:</h2>
+            <div>
+                <input type="search" placeholder="Hilton Hotel" />
+            </div>
             <form>
                 <Slider.Root className="SliderRoot" value={[maxPrice]} min={minPrice} max={270000} step={1000} onValueChange={handlePriceChange}>
                     <Slider.Track className="SliderTrack">
@@ -35,6 +57,45 @@ const HotelFilterSystem = ({ setSortBy }) => {
                 <p>MinPrice: {minPrice}</p>
                 <p>MaxPrice: {maxPrice}</p>
             </div>
+
+            <h3>Amenities</h3>
+            <form className="amenities-filter">
+                <label>
+                    <input type="checkbox" value="Air conditioning" onChange={handleAmenitiesChange} />
+                    Air conditioning
+                </label>
+                <label>
+                    <input type="checkbox" value="Bar/lounge" onChange={handleAmenitiesChange} />
+                    Bar/lounge
+                </label>
+                <label>
+                    <input type="checkbox" value="Security" onChange={handleAmenitiesChange} />
+                    Security
+                </label>
+                <label>
+                    <input type="checkbox" value="Kitchen" onChange={handleAmenitiesChange} />
+                    Kitchen
+                </label>
+            </form>
+            <h4>Property Rating</h4>
+            <form className="rating-filter">
+            <label>
+                <input type="checkbox" value="2" onChange={e => handleRatingSelection(e)} />
+                2 Stars
+            </label>
+            <label>
+                <input type="checkbox" value="3" onChange={e => handleRatingSelection(e)} />
+                3 Stars
+            </label>
+            <label>
+                <input type="checkbox" value="4" onChange={e => handleRatingSelection(e)} />
+                4 Stars
+            </label>
+            <label>
+                <input type="checkbox" value="5" onChange={e => handleRatingSelection(e)} />
+                5 Stars
+            </label>
+            </form>
         </div>
 
     )
